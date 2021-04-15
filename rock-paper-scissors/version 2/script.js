6c96c98c4604d4de0db4088cae58434b9b5cddd4
Antreas <antreasau79@outlook.com>
2021-04-15 09:38:52 +0300

    Last functions and event listeners

--- rock-paper-scissors/version 2/script.js
+++ rock-paper-scissors/version 2/script.js
@@ -18,7 +18,7 @@ const playerInput = dod(".playerInput");
 let pointsPlayer = 0;
 let pointsComputer = 0;
 const playerH4Rock = id("playerH4Rock");
-const playerH4Paper = id("playerH4Paper");  
+const playerH4Paper = id("playerH4Paper");
 const playerH4Scissors = id("playerH4Scissors");
 const computerH4Rock = id("computerH4Rock");
 const computerH4Paper = id("computerH4Paper");
@@ -30,7 +30,7 @@ let scoreComputer = dod(".scoreComputer");
 let computerHand = [];
 function computerPlay() {
     // Make first an array that contains rock, paper, scissors
-    const hand = ["rock","paper","scissors"];
+    const hand = ["rock", "paper", "scissors"];
     const random = Math.floor(Math.random() * hand.length);
     computerHand.push(hand[random]);
     return hand[random];
@@ -42,7 +42,7 @@ function turnAllImgDisplayToNone() {
     playerH4Rock.firstElementChild.style.display = "none";
     playerH4Paper.firstElementChild.style.display = "none";
     playerH4Scissors.firstElementChild.style.display = "none";
-    
+
     computerH4Rock.firstElementChild.style.display = "none";
     computerH4Paper.firstElementChild.style.display = "none";
     computerH4Scissors.firstElementChild.style.display = "none";
@@ -50,98 +50,116 @@ function turnAllImgDisplayToNone() {
 
 
 // make a function that plays rock paper scissors
-function playRound(playerSelection,computerSelection) {
+function playRound(playerSelection, computerSelection) {
     // Take the parameters in to variables and make them case insensitive
     let computer = computerSelection;
-    let playerRaw = playerSelection ;
+    let playerRaw = playerSelection;
     let player = playerRaw.toLowerCase();
 
     turnAllImgDisplayToNone();
     playerDisplayPhotos(player);
     computerDisplayPhotos(computer);
-    winLose(computer,player)
-    displayScore(pointsPlayer,pointsComputer);
+    winLose(computer, player)
+    displayScore(pointsPlayer, pointsComputer);
     console.log(pointsPlayer);
-    }
+}
 
 
 
+let result = id("divDisplayResult");
+// a function that decides who wins
+function winLose(computer, player) {
+    // write an if statment that says rock wins against scissors, scissors wins against paper, paper wins against rock and to give or take points
+    if (computer == "rock" && player == "scissors") {
+        pointsPlayer--, pointsComputer++;
+        return result.innerText = "You Lose! Rock beats Scissor";
+    } else if (computer == "scissors" && player == "rock") {
+        pointsPlayer++, pointsComputer--;
+        return result.innerText = "You Win! Rock beats Scissor";
+    } else if (computer == "scissors" && player == "paper") {
+        pointsPlayer--, pointsComputer++;
+        return result.innerText = "You Lose! Scissor beats Paper";
+    } else if (computer == "paper" && player == "scissors") {
+        pointsPlayer++, pointsComputer--;
+        return result.innerText = "You Win! Scissor beats Paper";
+    } else if (computer == "paper" && player == "rock") {
+        pointsPlayer--, pointsComputer++;
+        return result.innerText = "You Lose! Paper beats Rock";
+    } else if (computer == "rock" && player == "paper") {
+        pointsPlayer++, pointsComputer--;
+        return result.innerText = "You Win! Paper beats Rock";
+    } else if (computer == "rock" && player == "rock") {
+        return result.innerText = "Draw!";
+    } else if (computer == "paper" && player == "paper") {
+        return result.innerText = "Draw!";
+    } else if (computer == "scissors" && player == "scissors") {
+        return result.innerText = "Draw!";
+    } else {
+        console.log("Nothing");
+    };
+    console.log("hello");
+}
 
-    // a function that decides who wins
-    function winLose(computer,player) {
-        // write an if statment that says rock wins against scissors, scissors wins against paper, paper wins against rock and to give or take points
-        if (computer == "rock" && player == "scissors") {
-            pointsPlayer--,pointsComputer++;
-            return "You Lose! Rock beats Scissor";
-        } else if (computer == "scissors" && player == "rock") {
-            pointsPlayer++,pointsComputer--;
-            return "You Win! Rock beats Scissor";
-        } else if (computer == "scissors" && player == "paper") {
-            pointsPlayer--,pointsComputer++;
-            return "You Lose! Scissor beats Paper";
-        } else if (computer == "paper" && player == "scissors") {
-            pointsPlayer++,pointsComputer--;
-            return "You Win! Scissor beats Paper";
-        } else if (computer == "paper" && player == "rock") {
-            pointsPlayer--,pointsComputer++;
-            return "You Lose! Paper beats Rock";
-        } else if (computer == "rock" && player == "paper") {
-            pointsPlayer++,pointsComputer--;
-            return "You Win! Paper beats Rock";
-        } else if (computer == "rock" && player == "rock") {
-            return "Draw!";
-        } else if (computer == "paper" && player == "paper") {
-            return "Draw!";
-        } else if (computer == "scissors" && player == "scissors") {
-            return "Draw!";
-        }
-        console.log( "hello")
-    }
-
-
-    // Make a function that adds an event listener to the btn and plays (playRound)
-function AddEventOnBtn(a,b) {
+// Make a function that adds an event listener to the btn and plays (playRound)
+function AddEventOnBtn(a, b) {
     return a.addEventListener("click", (event) => {
-        playRound(b.innerText,computerPlay())
-        
+        playRound(b.innerText, computerPlay());
+
     })
 }
-    function playerDisplayPhotos(playerChoise) {
-         // make an if statment that chose the correct photo to display
-        if (playerChoise == "rock") {
-                return playerH4Rock.firstElementChild.style.display = "inherit";
-            } else if (playerChoise == "paper") {
-                return playerH4Paper.firstElementChild.style.display = "inherit";
-            } else if (playerChoise == "scissors") {
-                return playerH4Scissors.firstElementChild.style.display = "inherit";
-            }
+function playerDisplayPhotos(playerChoise) {
+    // make an if statment that chose the correct photo to display
+    if (playerChoise == "rock") {
+        return playerH4Rock.firstElementChild.style.display = "inherit";
+    } else if (playerChoise == "paper") {
+        return playerH4Paper.firstElementChild.style.display = "inherit";
+    } else if (playerChoise == "scissors") {
+        return playerH4Scissors.firstElementChild.style.display = "inherit";
     }
+}
 
-    function computerDisplayPhotos(computerChoise)  {     
-        // make an if statment that chose the correct photo to display
-        if (computerChoise == "rock") {
-            return computerH4Rock.firstElementChild.style.display = "inherit";
-        } else if (computerChoise == "paper") {
-            return computerH4Paper.firstElementChild.style.display = "inherit";
-        } else if (computerChoise == "scissors") {
-            return computerH4Scissors.firstElementChild.style.display = "inherit";
-        }
+function computerDisplayPhotos(computerChoise) {
+    // make an if statment that chose the correct photo to display
+    if (computerChoise == "rock") {
+        return computerH4Rock.firstElementChild.style.display = "inherit";
+    } else if (computerChoise == "paper") {
+        return computerH4Paper.firstElementChild.style.display = "inherit";
+    } else if (computerChoise == "scissors") {
+        return computerH4Scissors.firstElementChild.style.display = "inherit";
     }
+}
 
-    // make a function that takes the points of the players and displays it 
-    function displayScore( Player,Computer) {
-        scorePlayer.innerText =   Player;
-        scoreComputer.innerText = Computer;
+// make a function that takes the points of the players and displays it 
+function displayScore(Player, Computer) {
+    scorePlayer.innerText = Player;
+    scoreComputer.innerText = Computer;
+}
+
+
+
+
+
+
+AddEventOnBtn(btnRock, btnRock);
+AddEventOnBtn(btnPaper, btnPaper);
+AddEventOnBtn(btnScissors, btnScissors);
+
+
+
+const displayWon = id("youWon");
+const displayLost = id("youLose");
+const btnPlayAgain = document.querySelectorAll(".btnPlayAgain");
+
+// make a if statment that stops the game if one of the two player wins 
+document.addEventListener("click" , (event) => {
+    if (pointsPlayer == 5) {
+        return displayWon.style.display = "inherit";
+    } else if (pointsComputer == 5) {
+        return displayLost.style.display = "inherit";
     }
-
-    
-
-
-
-
-AddEventOnBtn(btnRock,btnRock);
-AddEventOnBtn(btnPaper,btnPaper);
-AddEventOnBtn(btnScissors,btnScissors);
-
-// make a function that shows the winer
-//make a function that shows the return from the if statment
+})
+for (let i = 0; i < btnPlayAgain.length; i++) {
+btnPlayAgain[i].addEventListener("click", (event) => {
+    location.reload();
+})
+}
